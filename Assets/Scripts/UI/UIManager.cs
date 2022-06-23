8bff3738006a15bf6c2c4c7bb8d16336c6fc38f1
DefoltFES <mironoff.emil@yandex.ru>
2022-06-23 09:15:02 +0300

    little fix

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,12 +9,12 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class UIManager : NetworkSingleton<UIManager>
 {
     public NetworkVariable<bool> timerActive = new NetworkVariable<bool>();
     private NetworkVariable<TimeSpan> currentTime = new NetworkVariable<TimeSpan>();
-    private NetworkVariable<TimeSpan> startTime = new NetworkVariable<TimeSpan>();
     public int startMinutes;
     public TextMeshProUGUI currentTimeText;
     public GameObject Buttons;
@@ -38,10 +38,10 @@ public class UIManager : NetworkSingleton<UIManager>
         {
             if (timerActive.Value)
             {
-                UpdateTimeGameServerRpc();
+                currentTime.Value -= TimeSpan.FromSeconds(Time.deltaTime);
                 if (currentTime.Value <= TimeSpan.Zero)
                 {
-                    StopTimeServerRpc();
+                    StopTime();
                     SceneLoaderWrapper.Instance.LoadScene("Scene_EndGame", true);
                 }
             }
@@ -49,41 +49,33 @@ public class UIManager : NetworkSingleton<UIManager>
         currentTimeText.text = currentTime.Value.ToString(@"mm\:ss");
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    void UpdateTimeGameServerRpc()
-    {
-        currentTime.Value -= TimeSpan.FromSeconds(Time.deltaTime);
-        startTime.Value += TimeSpan.FromSeconds(Time.deltaTime);
-    }
+
 
     [ServerRpc(RequireOwnership = false)]
     void StartTimeServerRpc()
     {
         timerActive.Value = true;
         currentTime.Value = TimeSpan.FromSeconds(startMinutes * 60);
-        startTime.Value = TimeSpan.Zero;
         Buttons.SetActive(true);
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    public void StopTimeServerRpc()
+
+    public void StopTime()
     {
+        foreach (var person in NetworkManager.Singleton.ConnectedClients)
+        {
+            person.Value.PlayerObject.GetComponent<ThirdPersonController>().isTag.Value = false;
+        }
         timerActive.Value = false;
         SpawnManager.loudingCount.Value = 0;
+       
     }
 
     public void ExitGame()
     {
         if (IsServer)
         {
-            var player = NetworkManager.SpawnManager.GetPlayerNetworkObject(NetworkManager.LocalClientId)
-                .GetComponent<ThirdPersonController>().isTag;
-            if (player.Value)
-            {
-                player.Value = false;
-            }
-            StopTimeServerRpc();
-
+            StopTime();
             SceneLoaderWrapper.Instance.LoadScene("Scene_EndGame", true);
         }
         else
